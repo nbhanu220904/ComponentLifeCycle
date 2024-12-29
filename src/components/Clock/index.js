59b7e8092fdb678ede29b5ba5e8f8e88e62b1a72
bhanu <n.bhanudurgaprasad2209@gmail.com>
2024-12-29 17:56:10 +0000

    6th Commmit

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -2,10 +2,20 @@ import {Component} from 'react'
 import './index.css'
 
 class Clock extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+  }
+
   render() {
+    const {date} = this.state
+    console.log(date)
+
     return (
       <div className="clock-container">
-        <h1 className="heading">Clock</h1>
+        <h1 className="heading">Clock </h1>
+        <p className="date">{date.toLocaleDateString()}</p>
+        <p className="time">{date.toLocaleTimeString()}</p>
       </div>
     )
   }
